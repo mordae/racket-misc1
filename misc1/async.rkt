40be5066dc6dbd3caf2b9737ad5b9d57ee02f480
Jan Dvořák <mordae@anilinux.org>
2014-08-01 17:48:37 +0200

    async: added async/loop
    The `async/loop` will keep running and producing values indefinitely.
    
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- misc1/async.rkt
+++ misc1/async.rkt
@@ -5,15 +5,18 @@
 
 (require racket/contract)
 
-(require "syntax.rkt")
+(require "syntax.rkt"
+         "evt.rkt")
 
-(provide async)
+(provide async
+         async/loop)
 
 (provide
   (contract-out
     (async-task? predicate/c)
     (async-task-send (-> async-task? any/c void?))
-    (rename async-proc async-task (-> (-> any) async-task?))))
+    (rename async-proc async-task (-> (-> any) async-task?))
+    (rename async-loop-proc async-task/loop (-> (-> any) async-task?))))
 
 
 (struct success
@@ -23,14 +26,28 @@
   (exception))
 
 (struct async-task
-  (evt thread)
+  (thread evt)
   #:property prop:evt (struct-field-index evt))
 
 
 (define (async-proc proc)
-  (let* ((result #f)
-         (thread (thread (λ_ (set! result (call/wrap proc))))))
-    (async-task (wrap-evt thread (λ_ (unwrap result))) thread)))
+  (let* ((channel (make-channel))
+         (thread (thread (λ ()
+                           (channel-put channel (call/wrap proc))))))
+    (async-task thread
+                (wrap-evt (cache-evt channel)
+                          (λ (result)
+                            (unwrap result))))))
+
+(define (async-loop-proc proc)
+  (let* ((channel (make-channel))
+         (thread (thread (λ ()
+                           (loop
+                             (channel-put channel (call/wrap proc)))))))
+    (async-task thread
+                (wrap-evt channel
+                          (λ (result)
+                            (unwrap result))))))
 
 (define (call/wrap proc)
   (with-handlers ((void (λ (exn)
@@ -43,13 +60,18 @@
       (apply values (success-values result))
       (raise (failure-exception result))))
 
+(define (async-task-send at v)
+  (let ((thread (async-task-thread at)))
+    (thread-send thread v)))
+
+
 (define-syntax-rule (async body ...)
   (async-proc
     (λ () body ...)))
 
-(define (async-task-send at v)
-  (let ((thread (async-task-thread at)))
-    (thread-send thread v)))
+(define-syntax-rule (async/loop body ...)
+  (async-loop-proc
+    (λ () body ...)))
 
 
 ; vim:set ts=2 sw=2 et:
