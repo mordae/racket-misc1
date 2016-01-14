92d66c9c2c5fefe4762acc221b69c5e716a6873d
Jan Dvořák <mordae@anilinux.org>
2016-01-14 10:11:33 +0100

    Name scribble-lib as a built-time dependency
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "unstable-lib" "scribble-lib"))
-(define build-deps '("racket-doc" "unstable-lib" "unstable-doc"))
+(define deps '("base" "unstable-lib"))
+(define build-deps '("racket-doc" "unstable-lib" "unstable-doc" "scribble-lib"))
 
 ; vim:set ts=2 sw=2 et:
